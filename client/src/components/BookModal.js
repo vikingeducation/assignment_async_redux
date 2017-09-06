2c6b98dfd9ea40333c6c8c8e7f2e38e16185e169
Greg Filipczak <gregfilipczak@gmail.com>
2017-09-06 22:12:09 +0000

    ugly modal

--- client/src/components/BookModal.js
+++ client/src/components/BookModal.js
@@ -1,11 +1,31 @@
 import React from "react";
+import "./BookModal.css";
 
 const BookModal = ({ bookModal, book, closeModal }) => {
   if (!bookModal) return null;
   return (
-    <div>
+    <div id="modal">
       <h1 onClick={closeModal}>X</h1>
-      <h1>{book.title}</h1>
+      <h1>
+        {book.title}
+      </h1>
+      <h2>
+        {book.authors.map(author => author)}
+      </h2>
+      <img src={book.image} />
+      <p>
+        Publication year: {book.publicationYear} <br /> Publisher:{" "}
+        {book.publisher}
+      </p>
+      <p>
+        {book.description}
+      </p>
+      <p>
+        Purchase on {book.buyLinkName} <a href={book.buyLink}>here</a>
+      </p>
+      <p>
+        GoodReads rating: {book.averageRating}
+      </p>
     </div>
   );
 };
