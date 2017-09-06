3b8ce2e1d4924cbf73d1fb04d6d48839b8e351e6
Eric Glover <etgt28@mail.missouri.edu>
2017-09-06 10:52:25 -0500

    setting up server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,56 @@
-require('dotenv').config()
+const express = require("express");
+const app = express();
+require("dotenv").config({ path: "../" });
+const API_CLIENT_KEY = process.env.API_CLIENT_KEY;
+const API_CLIENT_SECRET = process.env.API_CLIENT_SECRET;
 
-const API_CLIENT_ID = process.env.API_CLIENT_ID
- 
+const port = 3001;
+const baseUrl = `https://www.goodreads.com`;
+
+function checkStatus(response) {
+  if (!response.ok) {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+
+  // Otherwise just return the response
+  return response;
+}
+
+//handle some errors
+function errorHandler(err, req, res, next) {
+  console.log(`Error: ${err.stack}`);
+  res.status(err.response ? err.response.status : 500);
+  res.json({ error: err.message });
+}
+
+// Tell the app to use the errorHandler middleware
+app.use(errorHandler);
+
+const parseString = require("xml2js").parseString;
+// const xml2JSON = parseString(xml, function(err, result) {
+//   return result;
+// });
+
+//routes
+app.get("/api/stuff", (req, res, next) => {
+  const searchParams = "stuff";
+  fetch(`${baseUrl}search/index.xml?key=${API_CLIENT_KEY}&q=${searchParams}`)
+    .then(response => {
+      return parseString(response, () => {
+        return response;
+      });
+    })
+    .then(checkStatus)
+    .then(json => {
+      res.json(json);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Find the server at http://localhost:${port}/`);
+});
