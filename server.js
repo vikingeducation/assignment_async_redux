965c2a6b0a15594ae35bce0c0019c4de81f308ec
iandavid <iandavid@iandavids-MacBook-Pro.local>
2017-09-06 11:53:38 -0400

    problem with xml

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
+const parseString = require("xml2js").parseString;
 
 require("dotenv").config();
 const GOODREADS_KEY = process.env.GOODREADS_KEY;
 const GOODREADS_SECRET = process.env.GOODREADS_SECRET;
-const BASE_URL = "https://www.goodreads.com/search/index.xml";
+const baseUrlSearch = query =>
+  `https://www.goodreads.com/search/index.xml?key=${GOODREADS_KEY}&q=${query}`;
+const baseUrlShowBook = book =>
+  `https://www.goodreads.com/book/show/${book}?key=${GOODREADS_KEY}`;
 
-app.get("/goodreads/title", (req, res, next) => {});
+app.get("/goodreads/:query", async (req, res, next) => {
+  const query = req.params.query;
+  let xmlRes = await fetch(baseUrlSearch(query));
+  xmlRes = await xmlRes.text();
+  let jsonRes = await parseString(xmlRes);
+  console.log(jsonRes);
+});
 
-app.get("/goodreads/author", (req, res, next) => {});
-
-app.get("/goodreads/bookID", (req, res, next) => {
-  //URL: https://www.goodreads.com/book/show.FORMAT  (sample url)
-  // HTTP method: GET
-  // Parameters:
-  // format: xml or json
-  // key: Developer key (required).
-  // id: A Goodreads internal book_id
-  // text_only: Only show reviews that have text (default false)
-  // rating: Show only reviews with a particular rating (optional)
+app.get("/goodreads/show/:book", async (req, res, next) => {
+  const book = req.params.book;
+  const xmlRes = await fetch(baseUrlShowBook(book));
+  const jsonRes = await parseString(await xmlRes.text());
+  console.log(jsonRes);
 });
 
 app.listen(3001, "0.0.0.0", (req, res) => {
-  console.log("listening on port 3000");
+  console.log("listening on port 3001");
 });
