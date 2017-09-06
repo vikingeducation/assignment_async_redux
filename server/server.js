90e71a0f840397a45110f576d861b6b5f28c1a10
Eric Glover <etgt28@mail.missouri.edu>
2017-09-06 11:45:20 -0500

    testing server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+// require("es6-promise").polyfill;
+require("isomorphic-fetch");
 const express = require("express");
 const app = express();
-require("dotenv").config({ path: "../" });
+require("dotenv").config({ path: "../.env" });
 const API_CLIENT_KEY = process.env.API_CLIENT_KEY;
 const API_CLIENT_SECRET = process.env.API_CLIENT_SECRET;
 
@@ -8,11 +10,12 @@ const port = 3001;
 const baseUrl = `https://www.goodreads.com`;
 
 function checkStatus(response) {
-  if (!response.ok) {
-    const error = new Error(response.statusText);
-    error.response = response;
-    throw error;
-  }
+  console.log("res = ", response);
+  // if (!response.ok) {
+  //   const error = new Error(response.statusText);
+  //   error.response = response;
+  //   throw error;
+  // }
 
   // Otherwise just return the response
   return response;
@@ -28,18 +31,25 @@ function errorHandler(err, req, res, next) {
 // Tell the app to use the errorHandler middleware
 app.use(errorHandler);
 
+//handle XML
 const parseString = require("xml2js").parseString;
 // const xml2JSON = parseString(xml, function(err, result) {
 //   return result;
 // });
 
 //routes
-app.get("/api/book_search_stuff", (req, res, next) => {
-  const searchParams = "stuff";
+app.get("/api/book", (req, res, next) => {
+  //no search params is edge case to be treated later
+  const searchParams = req.query.search || "bible";
+  console.log(
+    "url =  ",
+    `${baseUrl}/search/index.xml?key=${API_CLIENT_KEY}&q=${searchParams}`
+    // `${baseUrl}/search/index.xml?key=${API_CLIENT_KEY}{q}`
+  );
   fetch(`${baseUrl}/search/index.xml?key=${API_CLIENT_KEY}&q=${searchParams}`)
     .then(response => {
-      return parseString(response, () => {
-        return response;
+      return parseString(response, data => {
+        return data;
       });
     })
     .then(checkStatus)
@@ -51,16 +61,12 @@ app.get("/api/book_search_stuff", (req, res, next) => {
     });
 });
 
-app.get("/api/review_search_stuff", (req, res, next) => {
-  const bookId = //some thing
-  const searchParams = "stuff";
+//FORGET THE WIDGETS, GRAB OTHER INFO
+app.get("/api/review/:id", (req, res, next) => {
+  const bookId = req.params.id;
   fetch(`${baseUrl}/book/show/${bookId}.json?key=${API_CLIENT_KEY}`)
-    .then(response => {
-      return parseString(response, () => {
-        return response;
-      });
-    })
     .then(checkStatus)
+    .then(JSON.parse)
     .then(json => {
       res.json(json);
     })
@@ -70,5 +76,5 @@ app.get("/api/review_search_stuff", (req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Find the server at http://localhost:${port}/`);
+  console.log(`Listening on http://localhost:${port}/`);
 });
