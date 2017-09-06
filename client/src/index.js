df6100d32776c8215b6867a18b113878efe3aa2a
Stephanie Barker <song_mage@yahoo.com>
2017-09-06 11:49:02 -0400

    hooking up bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "jquery/src/jquery";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
