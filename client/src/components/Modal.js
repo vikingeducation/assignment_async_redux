702b2d929694cba04471fec72a319609a74d5782
Alex <rollinglex@gmail.com>
2017-11-21 05:52:32 +0900

    added authors to modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,24 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
+let bookAuthors = authorsArray => {
+	return authorsArray.map((item, i) => (
+		<p key={i} className="modal-author">
+			{item}
+		</p>
+	));
+};
 
 const Modal = props => {
 	const { display, book, onClick } = props;
 	const modalStatus = display ? "block" : "none";
+	const authorsArray = book.authors;
+	console.log("AUTHORS_ARRAY", authorsArray);
+	console.log("AUTHORRS", bookAuthors(authorsArray));
+	function createMarkup() {
+		return { __html: book.description };
+	}
+
 	return (
 		<div
 			style={{ display: modalStatus }}
 			id="modal-wrapper"
 			onClick={e => onClick()}>
-			`HERRO MODAL {modalStatus}`
 			<div className="modal">
 				<div id="modal-header">
 					<img src={book.cover} alt="book cover" />
-					<p id="modal-title">{book.title}</p>
-					<p>{book.author}</p>
+					<div id="modal-title-container">
+						<p id="modal-title">{book.title}</p>
+						<div id="modal-author-container">{bookAuthors(authorsArray)}</div>
+					</div>
 				</div>
-				<div id="book-description">
-					<p>Description: </p> {book.description}
-				</div>
+				<div id="book-description" dangerouslySetInnerHTML={createMarkup()} />
 			</div>
 		</div>
 	);
